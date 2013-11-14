97e932586b89ea0c7b68d9f20c04a4fa5da748cc
Leopoldo Rivas <leopoldo@worket.co>
2013-11-14 18:59:45 -0300

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spotsor::Application.config.secret_key_base = '9c10e4809213b271e56ecb0382b1b30c5d3d87accdb4ac21fa050e0aafe071558eddb7632fbb1352b2bb13bad1b9af6ab6623fa19d7f453901f678c0db544967'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Spotsor::Application.config.session_store :cookie_store, key: '_spotsor_session'
