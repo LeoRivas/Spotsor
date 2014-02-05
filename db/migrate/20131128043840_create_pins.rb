0fd70a9848d28ac5309836d55039a8235f96a687
Leopoldo Rivas <leopoldo@worket.co>
2014-02-05 19:33:47 -0300

    photos upload with carrierwave

--- db/migrate/20131128043840_create_pins.rb
+++ db/migrate/20131128043840_create_pins.rb
@@ -2,7 +2,7 @@ class CreatePins < ActiveRecord::Migration
   def change
     create_table :pins do |t|
       t.text :description
-      t.text :photo
+      t.file :photo
 
       t.timestamps
     end
